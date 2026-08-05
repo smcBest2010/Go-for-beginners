29bcb0efa5e316926c5e83476f9e350b1231b7dc
m161awm2 <kubonmo519@gmail.com>
2026-08-05 23:39:21 +0900

    feat: 필요한 주석을 추가.

--- 01-hello-world/something/something.go
+++ 01-hello-world/something/something.go
@@ -2,10 +2,10 @@ package something
 
 import "fmt"
 
-func sayBye() {
+func sayBye() { // 함수를 소문자로 시작하면 외부 패키지에서 접근 불가능함. 즉, private 함수임
 	fmt.Println("Bye")
 }
 
-func SayHello() {
+func SayHello() { // 함수를 대문자로 시작하면 외부 패키지에서 접근 가능함. 즉, public 함수임
 	fmt.Println("Hello")
 }
