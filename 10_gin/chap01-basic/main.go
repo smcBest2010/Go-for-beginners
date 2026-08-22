40e92047d727b9773b2e2a620339fea725115c90
m161awm2 <kubonmo519@gmail.com>
2026-08-22 21:04:59 +0900

    gin lore

--- 10_gin/chap01-basic/main.go
+++ 10_gin/chap01-basic/main.go
@@ -1,64 +1,14 @@
+// Gin앱 만들기
 package main
 
-import (
-	"net/http"
-
-	"github.com/gin-gonic/gin"
-)
-
-// JSON으로 받을 데이터 구조
-type User struct {
-	Name string `json:"name"`
-	Age  int    `json:"age"`
-}
+import "github.com/gin-gonic/gin"
 
 func main() {
-	// Gin Router 생성
-	r := gin.Default()
-
-	// GET /
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "Hello Gin",
+	router := gin.Default()
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
 		})
 	})
-
-	// GET /users/123
-	r.GET("/users/:id", func(c *gin.Context) {
-		id := c.Param("id")
-
-		c.JSON(http.StatusOK, gin.H{
-			"id": id,
-		})
-	})
-
-	// GET /search?name=kim
-	r.GET("/search", func(c *gin.Context) {
-		name := c.Query("name")
-
-		c.JSON(http.StatusOK, gin.H{
-			"name": name,
-		})
-	})
-
-	// POST /users
-	r.POST("/users", func(c *gin.Context) {
-		var user User
-
-		// 요청 JSON → User 구조체
-		if err := c.ShouldBindJSON(&user); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{
-				"error": "잘못된 JSON입니다.",
-			})
-			return
-		}
-
-		c.JSON(http.StatusCreated, gin.H{
-			"message": "유저 생성 완료",
-			"user":    user,
-		})
-	})
-
-	// 서버 실행
-	r.Run(":8080")
+	router.Run()
 }
